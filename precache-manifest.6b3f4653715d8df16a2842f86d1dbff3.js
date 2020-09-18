05c8e007c043c89a6b1e0681f47b4c973e5d848e
Bao Trong <bao.trong@microsolution.com.vn>
2020-09-18 20:34:51 +0700

    Updates

---
+++ precache-manifest.6b3f4653715d8df16a2842f86d1dbff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87dab8c23e78136f9c81d592bfd9309",
+    "url": "/ToDoReactApp/index.html"
+  },
+  {
+    "revision": "dc9dd4183b1c905623aa",
+    "url": "/ToDoReactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e1d4ed500e32d5475f5",
+    "url": "/ToDoReactApp/static/js/2.4c6b25ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoReactApp/static/js/2.4c6b25ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9dd4183b1c905623aa",
+    "url": "/ToDoReactApp/static/js/main.34d49866.chunk.js"
+  },
+  {
+    "revision": "5716201c3b5128b25e44",
+    "url": "/ToDoReactApp/static/js/runtime-main.606c41e7.js"
+  }
+]);
